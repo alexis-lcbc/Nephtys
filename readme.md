0683f291f395c8024a66f2e2e40966f06db91fd8
alexis-lcbc <alexis.lecabellec@gmail.com>
2025-08-26 03:48:41 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Nephtys
+Selfhosted Home Safety Camera software built with Rust, Svelte, FFMPEG & OpenCV
+Nephtys is designed for local deployement with a .local domain using zeroconf mDNS
+Nephtys can be deployed to capture almost any supported linux webcams.
+
+## Goals
+- [x] HLS Streaming one camera
+- [ ] Save last 4 days to disk
+- [ ] Able to record from one or more sources
+- [x] Movement and person detection
+- [ ] Automatic save when movement detection
+- [ ] Remote control (Web UI)
+    - Basic stream view (done)
+    - Event logs (movement detection)
+    - View past recordings
+- [ ] Home Assistant integration
+- [ ] Automatic push notifications alerts (via HA)
---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Hls from "hls.js";
+	import { onMount } from "svelte";
+
+    let video_elm: HTMLVideoElement | undefined = $state();
+    let video_src = "http://localhost:8080/static/stream/stream.m3u8"
+    onMount(() => {
+        if (Hls.isSupported() && video_elm) {
+            var hls = new Hls();
+            hls.loadSource(video_src);
+            hls.attachMedia(video_elm);
+        }
+    })
+</script>
+
+<div class="bg-gray-900 text-white w-full h-full p-5">
+    <div class="flex items-center justify-center flex-col">
+        <h1 class="text-4xl">Nephtys Camera Software</h1>
+        <!-- svelte-ignore a11y_media_has_caption -->
+        <video bind:this={video_elm} controls></video>
+    </div>
+</div>
